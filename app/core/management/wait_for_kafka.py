691a214e98529e28cc431b810e7fab08f53f9b46
Rute Souza <rutesouza6801@gmail.com>
2024-10-08 23:42:26 -0300

    update kafka script

--- app/core/management/wait_for_kafka.py
+++ app/core/management/wait_for_kafka.py
@@ -1,33 +1,32 @@
 import sys
-import asyncio
-from aiokafka import AIOKafkaProducer
+import time
+
+from kafka import KafkaAdminClient
+from kafka.errors import KafkaError
 
 BOOTSTRAP_SERVERS = 'kafka:29092'
 
 
-async def wait_for_kafka():
+def wait_for_kafka():
     """
     Wait until Kafka is available to start app.
     """
     max_attempts = 10
     attempt = 0
-
     while attempt < max_attempts:
         try:
-            producer = AIOKafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS)
-            await producer.start()
-            await producer.send_and_wait("test_topic", b"health_check")
-            await producer.stop()
+            admin_client = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVERS)
+            admin_client.list_topics()
+            admin_client.close()
             print("Kafka is ready")
             return
-        except Exception as e:
+        except KafkaError:
             attempt += 1
-            print(f"Kafka not ready, retrying... (attempt {attempt}) - Error: {e}")
-            await asyncio.sleep(5)
-
+            print("Kafka not ready, retrying...")
+            time.sleep(5)
     print("Kafka did not become ready in time")
     sys.exit(1)
 
 
 if __name__ == "__main__":
-    asyncio.run(wait_for_kafka())
+    wait_for_kafka()
