482bb7d14db97386402745d154bd72c683b03988
Rute Souza <rutesouza6801@gmail.com>
2024-10-13 16:14:33 -0300

    Validate category

--- app/transaction_history/schemas.py
+++ app/transaction_history/schemas.py
@@ -1,6 +1,21 @@
+from enum import Enum
 from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel
+
+
+class CategoryEnum(str, Enum):
+    home = "home"
+    other = "other"
+    transactions = "transactions"
+    food = "food"
+    education = "education"
+    personal = "personal"
+    communication = "communication"
+    entertainment = "entertainment"
+    health = "health"
+    transport = "transport"
+    tax = "tax"
 
 
 class TransactionHistorySchema(BaseModel):
@@ -10,7 +25,7 @@ class TransactionHistorySchema(BaseModel):
     transaction_type: str
     amount: float
     balance_after_transaction: Optional[float] = None
-    category: Optional[str] = None
+    category: CategoryEnum
 
     model_config = {
         "from_attributes": True
