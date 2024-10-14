0f830fe34824371169dd0bf7daa5bdf3ea70568f
Rute Souza <rutesouza6801@gmail.com>
2024-10-14 16:22:17 -0300

    Make ws connection work

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from contextlib import asynccontextmanager
 
 from app.core.database import init_db
@@ -10,7 +10,7 @@ from app.emotional_data.routes import router as emotional_data_router
 from app.kafka_client.routes import router as kafka_router
 from app.transaction_history.routes import router as transaction_history_router
 from app.credit_limit.routes import router as credit_limit_router
-from app.ws.routes import router as ws_router
+from app.ws.client import WebSocketClient
 
 
 @asynccontextmanager
@@ -32,9 +32,19 @@ app.include_router(emotional_data_router)
 app.include_router(kafka_router)
 app.include_router(transaction_history_router)
 app.include_router(credit_limit_router)
-app.include_router(ws_router)
 
 
 @app.get("/")
 def read_root():
     return {"message": "Go to http://localhost:8000/docs"}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await WebSocketClient.connect(websocket)
+    try:
+        while True:
+            # Keep the connection open and ready to receive messages
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        await WebSocketClient.disconnect(websocket)
